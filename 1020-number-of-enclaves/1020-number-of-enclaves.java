5a543115d77e20a5e08844b5bdad9b6978bfe989
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-07-14 13:15:36 +0530

    Time: 8 ms (69.75%), Space: 59.7 MB (59.76%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -15,32 +15,23 @@ class Solution {
         return cnt+1;
     }
     public int numEnclaves(int[][] board) {
-        int r = board.length, c = board[0].length,cnt=0;
+        int r = board.length, c = board[0].length,cnt=0,sol=0;
         int[][] v = new int[r][c];
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                v[i][j]=0;
-            }
-        }
-        for(int i=0;i<r;i++){
-            if(board[i][0]==1 && v[i][0]==0)cnt=dfs(i,0,board,v,cnt);
-            System.out.println("i: " + i + " j: " + 0 + " cnr: " + cnt );
-            if(board[i][c-1]==1 && v[i][c-1]==0)cnt=dfs(i,c-1,board,v,cnt);
-            System.out.println("i: " + i + " j: " + (c-1) + " cnr: " + cnt );
-        }
-        for(int i=1;i<c-1;i++){
-            if(board[0][i]==1 && v[0][i]==0)cnt=dfs(0,i,board,v,cnt);
-            System.out.println("i: " + 0 + " j: " + i + " cnr: " + cnt );
-            if(board[r-1][i]==1 && v[r-1][i]==0)cnt=dfs(r-1,i,board,v,cnt);
-            System.out.println("i: " + (r-1) + " j: " + i + " cnr: " + cnt );
-        }
-        int sol=0;
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
                 if(board[i][j]==1){
                     sol++;
                 }
+                v[i][j]=0;
             }
+        }
+        for(int i=0;i<r;i++){
+            if(board[i][0]==1 && v[i][0]==0)cnt=dfs(i,0,board,v,cnt);
+            if(board[i][c-1]==1 && v[i][c-1]==0)cnt=dfs(i,c-1,board,v,cnt);
+        }
+        for(int i=1;i<c-1;i++){
+            if(board[0][i]==1 && v[0][i]==0)cnt=dfs(0,i,board,v,cnt);
+            if(board[r-1][i]==1 && v[r-1][i]==0)cnt=dfs(r-1,i,board,v,cnt);
         }
         return sol-cnt;
     }
