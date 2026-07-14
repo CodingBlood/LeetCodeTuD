05597d38a0abee57fb466d39e000aabde50b0d86
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-07-14 12:45:22 +0530

    Time: 2 ms (72.9%), Space: 47.8 MB (57.2%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,6 +2,8 @@ class Solution {
     static final int[] h = new int[]{-1,0,1,0};
     static final int[] vr = new int[]{0,-1,0,1};
     void dfs(int i, int j, char[][] b, char[][] bs, int[][] v){
+        v[i][j]=1;
+        bs[i][j]='O';
         for(int k=0;k<4;k++){
             int m=i+h[k];
             int n=j+vr[k];
@@ -24,28 +26,12 @@ class Solution {
             }
         }
         for(int i=0;i<r;i++){
-            if(board[i][0]=='O' && v[i][0]==0){
-                v[i][0]=1;
-                dfs(i,0,board,bs,v);
-                bs[i][0]='O';
-            }
-            if(board[i][c-1]=='O' && v[i][c-1]==0){
-                v[i][c-1]=1;
-                dfs(i,c-1,board,bs,v);
-                bs[i][c-1]='O';
-            }
+            if(board[i][0]=='O' && v[i][0]==0)dfs(i,0,board,bs,v);
+            if(board[i][c-1]=='O' && v[i][c-1]==0)dfs(i,c-1,board,bs,v);
         }
         for(int i=1;i<c-1;i++){
-            if(board[0][i]=='O' && v[0][i]==0){
-                v[0][i]=1;
-                dfs(0,i,board,bs,v);
-                bs[0][i]='O';
-            }
-            if(board[r-1][i]=='O' && v[r-1][i]==0){
-                v[r-1][i]=1;
-                dfs(r-1,i,board,bs,v);
-                bs[r-1][i]='O';
-            }
+            if(board[0][i]=='O' && v[0][i]==0)dfs(0,i,board,bs,v);
+            if(board[r-1][i]=='O' && v[r-1][i]==0)dfs(r-1,i,board,bs,v);
         }
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
