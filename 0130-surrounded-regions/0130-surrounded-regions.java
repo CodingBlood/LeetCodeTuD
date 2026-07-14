e406e712e88d60ab82398de861f5ffe94ba148f6
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-07-14 12:47:46 +0530

    Time: 1 ms (99.84%), Space: 47.6 MB (74.73%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,16 +1,14 @@
 class Solution {
     static final int[] h = new int[]{-1,0,1,0};
     static final int[] vr = new int[]{0,-1,0,1};
-    void dfs(int i, int j, char[][] b, char[][] bs, int[][] v){
-        v[i][j]=1;
+    void dfs(int i, int j, char[][] b, char[][] bs){
         bs[i][j]='O';
         for(int k=0;k<4;k++){
             int m=i+h[k];
             int n=j+vr[k];
             if(m>=0 && n>=0 && m<=b.length-1 && n<=b[0].length-1){ 
-                if(b[m][n]=='O' && v[m][n]!=1){
-                    v[m][n]=1;
-                    dfs(m,n,b,bs,v);
+                if(b[m][n]=='O' && bs[m][n]!='O'){
+                    dfs(m,n,b,bs);
                     bs[m][n]='O';
                 }
             }
@@ -18,7 +16,6 @@ class Solution {
     }
     public void solve(char[][] board) {
         int r = board.length, c = board[0].length;
-        int[][] v = new int[r][c];
         char[][] bs = new char[r][c];
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
@@ -26,12 +23,12 @@ class Solution {
             }
         }
         for(int i=0;i<r;i++){
-            if(board[i][0]=='O' && v[i][0]==0)dfs(i,0,board,bs,v);
-            if(board[i][c-1]=='O' && v[i][c-1]==0)dfs(i,c-1,board,bs,v);
+            if(board[i][0]=='O' && bs[i][0]=='X')dfs(i,0,board,bs);
+            if(board[i][c-1]=='O' && bs[i][c-1]=='X')dfs(i,c-1,board,bs);
         }
         for(int i=1;i<c-1;i++){
-            if(board[0][i]=='O' && v[0][i]==0)dfs(0,i,board,bs,v);
-            if(board[r-1][i]=='O' && v[r-1][i]==0)dfs(r-1,i,board,bs,v);
+            if(board[0][i]=='O' && bs[0][i]=='X')dfs(0,i,board,bs);
+            if(board[r-1][i]=='O' && bs[r-1][i]=='X')dfs(r-1,i,board,bs);
         }
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
