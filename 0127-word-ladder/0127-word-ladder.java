87af1851f2165bf719c2d1ed88bf13954a66ea3f
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-07-22 17:12:46 +0530

    Time: 110 ms (21.2%), Space: 48.4 MB (43%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -13,17 +13,12 @@ class Solution {
         q.add(new P(bW,1));
         while(!q.isEmpty()){
             P t = q.poll();
-            // System.out.println("LOOPING for: " + t.w);
             for(int i=0;i<t.w.length();i++){
                 for(int j=0;j<26;j++){
                     StringBuilder sb = new StringBuilder(t.w);
                     sb.setCharAt(i, (char) ('a' + j));
-                    // System.out.println("checking: " + sb.toString());
-                    
                     if(wL.contains(sb.toString())){
-                        // System.out.println("In set");
                         if(eW.equals(sb.toString())){
-                            // System.out.println("solved with n: " + t.n+1);
                             return t.n+1;
                         }
                         q.add(new P(sb.toString(),t.n+1));
