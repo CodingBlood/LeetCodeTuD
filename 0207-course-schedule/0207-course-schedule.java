f4a25466c97d66b01bd2cf87414b682d35aaf4d4
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-08-21 16:17:42 +0530

    Time: 7 ms (56.26%), Space: 47.5 MB (12.61%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,33 +1,64 @@
 class Solution {
-    // DFS
-    public boolean dfs(int i, int[] v, List<List<Integer>> gp){
-        if(v[i]==2)return true;   // no cycle
-        if(v[i]==1)return false;  // cycle
+    // // DFS
+    // public boolean dfs(int i, int[] v, List<List<Integer>> gp){
+    //     if(v[i]==2)return true;   // no cycle
+    //     if(v[i]==1)return false;  // cycle
 
-        v[i]=1;
-        for(int j=0;j<gp.get(i).size();j++){
-            if(!dfs(gp.get(i).get(j),v,gp)){
-                return false;
-            }
-        }
-        v[i]=2;
-        return true;
-    }
+    //     v[i]=1;
+    //     for(int j=0;j<gp.get(i).size();j++){
+    //         if(!dfs(gp.get(i).get(j),v,gp)){
+    //             return false;
+    //         }
+    //     }
+    //     v[i]=2;
+    //     return true;
+    // }
+    // public boolean canFinish(int nC, int[][] prerequisites) {
+    //     List<List<Integer>> gp = new ArrayList<>();
+    //     for(int i=0;i<nC;i++){
+    //         gp.add(new ArrayList<Integer>());
+    //     }
+    //     for(int i=0;i<prerequisites.length;i++){
+    //         gp.get(prerequisites[i][0]).add(prerequisites[i][1]);
+    //     }
+    //     int[] v = new int[nC];
+    //     for(int i=0;i<nC;i++){
+    //         if(v[i]==0){
+    //             if(!dfs(i,v,gp)){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    // Kahn's Algo
     public boolean canFinish(int nC, int[][] prerequisites) {
         List<List<Integer>> gp = new ArrayList<>();
+        int[] deg = new int[nC];
         for(int i=0;i<nC;i++){
             gp.add(new ArrayList<Integer>());
         }
         for(int i=0;i<prerequisites.length;i++){
             gp.get(prerequisites[i][0]).add(prerequisites[i][1]);
+            deg[prerequisites[i][1]]++;
         }
-        int[] v = new int[nC];
+        Deque<Integer> s = new ArrayDeque<Integer>();
         for(int i=0;i<nC;i++){
-            if(v[i]==0){
-                if(!dfs(i,v,gp)){
-                    return false;
+            if(deg[i]==0)s.push(i);
+        }
+        while(!s.isEmpty()){
+            int t=s.pop();
+            for(int i=0;i<gp.get(t).size();i++){
+                deg[gp.get(t).get(i)]--;
+                if(deg[gp.get(t).get(i)]==0){
+                    s.push(gp.get(t).get(i));
                 }
             }
+        }
+        for(int i=0;i<nC;i++){
+            if(deg[i]!=0)return false;
         }
         return true;
     }
