7a3b45a3d6d70e5aea9830989ada452eb61544c3
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-09-04 21:16:02 +0530

    Time: 322 ms (5.78%), Space: 71.9 MB (19.58%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,44 +1,49 @@
 class Solution {
-    class DisjointSet{
-        List<Integer> parent = new ArrayList<>();
-        List<Integer> size = new ArrayList<>();
-        DisjointSet(int n){
+    class DSU{
+        List<Integer> parent=new ArrayList<>();; 
+        List<Integer> rank=new ArrayList<>();; 
+        DSU(int n){
             for(int i=0;i<n;i++){
                 parent.add(i);
-                size.add(0);
+                rank.add(0);
             }
         }
         public int UP(int node){
-            if(node==parent.get(node))return node;
-            int up = UP(parent.get(node));
-            parent.set(node, up);
+            int p = parent.get(node); 
+            if(node==p)return node;
+            int up = UP(p);
+            parent.set(node,up);
             return up;
         }
-        public void uBS(int u,int v){
-            int upu = UP(u);
-            int upv = UP(v);
-            if(upu==upv)return;
-            if(size.get(upu)<size.get(upv)){
-                parent.set(upu,upv);
-                size.set(upv,size.get(upu)+size.get(upv));
+        public void UbyRank(int u,int v){
+            int uup = UP(u);
+            int vup = UP(v);
+            if(uup==vup)return;
+            if(rank.get(uup)>rank.get(vup)){
+                parent.set(vup,uup);
+                rank.set(vup,rank.get(uup)+rank.get(vup));
             }else{
-                parent.set(upv,upu);
-                size.set(upu,size.get(upu)+size.get(upv));
+                parent.set(uup,vup);
+                rank.set(uup,rank.get(uup)+rank.get(vup));    
             }
         }
     }
     public int makeConnected(int n, int[][] connections) {
         if(connections.length<n-1)return -1;
-        DisjointSet DSU = new DisjointSet(n);
+        DSU dsu = new DSU(n);
         for(int i=0;i<connections.length;i++){
-            DSU.uBS(connections[i][0],connections[i][1]);
+            dsu.UbyRank(connections[i][0],connections[i][1]);
         }
-        for(int i:DSU.parent){
-            System.out.println(i);
+        for(int i:dsu.parent){
+            System.out.print(i + " ");
+        }
+        System.out.println("i");
+        for(int i:dsu.rank){
+            System.out.print(i + " ");
         }
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(i==DSU.UP(i))cnt++;
+            if(i==dsu.UP(i))cnt++;
         } 
         
         return cnt-1;
