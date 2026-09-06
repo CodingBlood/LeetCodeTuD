9b421db7542570f867a33a86a7a40d3566426809
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-09-06 22:00:47 +0530

    Time: 42 ms (19.58%), Space: 50.6 MB (20.54%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,74 +1,62 @@
 class Solution {
-    class DisjointSet{
-        int[] parent,size;
-        public DisjointSet(int n){
-            parent = new int[n];
-            size = new int[n];
+    class DSU{
+        List<Integer> parent = new ArrayList<>();
+        List<Integer> rank   = new ArrayList<>();
+        DSU(int n){
             for(int i=0;i<n;i++){
-                parent[i]=i;
-                size[i]=1;
+                parent.add(i);
+                rank.add(0);
             }
         }
-        public int ultimateParent(int i){
-            if(parent[i]!=i){
-                parent[i] = ultimateParent(parent[i]);
-                return parent[i];
-            }
-            return i;
+        public int UP(int node){
+            if(parent.get(node)==node)return node;
+            int ultimate_parent=UP(parent.get(node));
+            parent.set(node,ultimate_parent);
+            return ultimate_parent;
         }
-
-        public void Union(int i, int j){
-            int rooti = ultimateParent(i);
-            int rootj = ultimateParent(j);
-            if(rooti!=rootj){
-                int ranki = size[rooti];
-                int rankj = size[rootj];
-                if(ranki>=rankj){
-                    parent[rootj]=rooti;
-                    size[rooti]+=size[rootj];
-                }else{
-                    parent[rooti]=rootj;
-                    size[rootj]+=size[rooti];
-                }
+        public void UnionbyRank(int u, int v){
+            int uup=UP(u);
+            int vup=UP(v);
+            if(uup==vup)return;
+            if(rank.get(uup)>=rank.get(vup)){
+                parent.set(vup,uup);
+                rank.set(uup, rank.get(uup)+rank.get(vup));
+            }else{
+                parent.set(uup,vup);
+                rank.set(vup, rank.get(uup)+rank.get(vup));
             }
-        }
-        public int[] parent(){
-            return parent;
         }
     }
-
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        HashMap<String, Integer> eToId = new HashMap<>();
-        HashMap<String, String> eToName = new HashMap<>();
+        Map<String, Integer> eToID   = new HashMap<String, Integer>();
+        Map<String, String > eToName = new HashMap<String, String >();
+        int n = accounts.size();
         int k=0;
-        for(List<String> account: accounts){
-            String name = account.get(0);
-            for(int j=1;j<account.size();j++){
-                String email = account.get(j);
-                if(!eToId.containsKey(account.get(j))){
-                    eToId.put(email,k++);
+        for(int i=0;i<n;i++){
+            String name=accounts.get(i).get(0);
+            for(int j=1;j<accounts.get(i).size();j++){
+                String email = accounts.get(i).get(j);
+                if(!eToID.containsKey(email)){
+                    eToID.put(email,k++);
                     eToName.put(email,name);
                 }
-                
-            }            
-        }
-        DisjointSet DSU = new DisjointSet(k);
-        for(List<String> acc : accounts){
-            if(acc.size()<2)continue;
-            String name = acc.get(0);
-            String fE = acc.get(1);
-            int fId = eToId.get(fE);
-            for(int j=2;j<acc.size();j++){
-                String nE = acc.get(j);
-                int nId = eToId.get(nE);
-                DSU.Union(fId,nId);
             }
         }
-
+        DSU dsu = new DSU(k);
+        for(int i=0;i<n;i++){
+            if(accounts.get(i).size()<2)continue;
+            String name = accounts.get(i).get(0);
+            String fE = accounts.get(i).get(1);
+            int fEId = eToID.get(fE);
+            for(int j=2;j<accounts.get(i).size();j++){
+                int cId = eToID.get(accounts.get(i).get(j));
+                dsu.UnionbyRank(fEId,cId);
+            }
+        }
         HashMap<Integer,List<String>> hmp = new HashMap<>();
-        for(String email: eToId.keySet()){
-            int id = eToId.get(email);
-            int up = DSU.ultimateParent(id);
+        for(String email: eToID.keySet()){
+            int id = eToID.get(email);
+            int up = dsu.UP(id);
             hmp.putIfAbsent(up, new ArrayList<>());
             hmp.get(up).add(email);
         }
