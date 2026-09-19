8d89edc2c7f233043758b0fff00b8aa9c69683da
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-09-20 04:59:58 +0530

    Time: 28 ms (96.25%), Space: 105.5 MB (5.02%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,43 +1,46 @@
 class MinStack {
     List<Integer> s = new ArrayList<>();
     List<Integer> min = new ArrayList<>();
-    int i=-1;
+    int i = -1;
+
     public MinStack() {
-        i=-1;
+        i = -1;
     }
-    
+
     public void push(int value) {
-        s.add(value);
         i++;
-        if(i-1>=0){
-            if(min.get(i-1)<value){
-                min.add(min.get(i-1));
-            }else{
-                min.add(value);
-            }
-        }else{
-            min.add(value);
+
+        int currentMin = value;
+        if (i > 0) {
+            currentMin = Math.min(min.get(i - 1), value);
         }
-        
+
+        if (s.size() > i) {
+            s.set(i, value);
+            min.set(i, currentMin);
+        } else {
+            s.add(value);
+            min.add(currentMin);
+        }
     }
-    
+
     public void pop() {
-        if(i>=0){
+        if (i >= 0) {
             s.remove(i);
             min.remove(i);
             i--;
         }
     }
-    
+
     public int top() {
-        if(i>=0){
+        if (i >= 0) {
             return s.get(i);
         }
         return 0;
     }
-    
+
     public int getMin() {
-        if(i>=0){
+        if (i >= 0) {
             return min.get(i);
         }
         return 0;
