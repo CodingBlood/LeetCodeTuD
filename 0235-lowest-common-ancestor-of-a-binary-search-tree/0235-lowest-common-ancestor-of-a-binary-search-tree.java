adff9a86d8532e1025053ec699a85e503c21a9e4
Kartik Agarwal <kartik.agarwal.192.168@gmail.com>
2026-09-22 11:39:56 +0530

    Time: 11 ms (0.78%), Space: 47.9 MB (17.18%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public boolean find(TreeNode root, TreeNode x){
+        if(root==x)return true;
+        if(root==null)return false;
+        return find(root.left,x) || find(root.right,x);
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode sol=root;
+        if(root==null)return null;
+        TreeNode l = lowestCommonAncestor(root.left,p,q);
+        TreeNode r = lowestCommonAncestor(root.right,p,q);
+        if(l!=null){
+            return l;
+        }else if(r!=null){
+            return r;
+        }else{
+            if(find(root,p) && find(root,q)){
+                return root;
+            }else{
+                return null;
+            }
+        }
+   
+        
+    }
+}
